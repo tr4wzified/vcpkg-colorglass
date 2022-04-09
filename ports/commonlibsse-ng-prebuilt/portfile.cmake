797767cd2ac6bcc09a0c1d85d057348295b9a27d
Charmed Baryon <dev@charmedbaryon.live>
2022-04-09 08:04:48 -0700

    Add CommonLibSSE NG v2.1.0.

--- ports/commonlibsse-ng-prebuilt/portfile.cmake
+++ ports/commonlibsse-ng-prebuilt/portfile.cmake
@@ -1,14 +1,14 @@
 if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
   vcpkg_download_distfile(COMMONLIB_DISTRO
-      URLS "https://vcpkg-colorglass-artifacts.s3.us-west-2.amazonaws.com/prebuilt/commonlibsse-ng_x64-windows-static-md_2.0.0.7z"
+      URLS "https://vcpkg-colorglass-artifacts.s3.us-west-2.amazonaws.com/prebuilt/commonlibsse-ng_x64-windows-static-md_2.1.0_0.7z"
       FILENAME commonlibsse.zip
-      SHA512 7a032c42e95db7eaa5af06bc2e06d7179bb69b0b0bcf794074681e17127d29b4c9f6a3620e9223487d84c55eeda05df7b06491ab520f3cd610cef3ecbad4cc17
+      SHA512 7727b28f0c77e93ada990536bb7e73b275e8756958a08b2a9f36514fa3fbb0573b85301f4b5d289921c9b3a0042b318b2d03d3ad042abf33d374858141d43027
   )
 else ()
   vcpkg_download_distfile(COMMONLIB_DISTRO
-      URLS "https://vcpkg-colorglass-artifacts.s3.us-west-2.amazonaws.com/prebuilt/commonlibsse-ng_x64-windows-static_2.0.0.7z"
+      URLS "https://vcpkg-colorglass-artifacts.s3.us-west-2.amazonaws.com/prebuilt/commonlibsse-ng_x64-windows-static_2.1.0_0.7z"
       FILENAME commonlibsse.zip
-      SHA512 b6e8328debad75a01697c7f5fb410b5e97ced6f82dc1ac838c479706855290a9d5512023299002d4e9efb907268fec41b25a85de768e211b6976f27b9fb5071d
+      SHA512 702744c90a1b76a3497abd22196d53fc5449cb62f002de6a941084f4aeb89f8c2263d3dd9fba47a1e8847b946d9d91093f71faf3aaa34268079c7487b20597bd
   )
 endif ()
 
