0e75fa9fffb186eab134c8ec9737e6dbd73bf55b
Charmed Baryon <dev@charmedbaryon.live>
2022-04-26 10:17:48 -0700

    Revert "Update CommonLibSSE NG to 3.2.0."
    This reverts commit 53d41fd14bd06769c5202edd2f854b78b2eb565c.

--- ports/commonlibsse-ng/portfile.cmake
+++ ports/commonlibsse-ng/portfile.cmake
@@ -1,14 +1,14 @@
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO CharmedBaryon/CommonLibSSE
-        REF 176c91eb50a28e5e6f6ff26ed25c31e3f5f33181
-        SHA512 916f71ea2c98baa562deb71077947e7b0bd70a7281bbbf5423138dbb9cc49038c41152590b370c6b95320b7b49d37fa3fc633128129df3028d4f81fa7b12339e
-        HEAD_REF dev-ng
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO CharmedBaryon/CommonLibSSE
+  REF 26fb2a29ca415845f3dafbeb60e21b171938a02d
+  SHA512 e4d2331fba332386fb058e033bc232ecb937c6c8d7886767dcb614da1b8eac3ab71dd87d7de054d70dc484e8887a75f09a87bd9e4d50f299e27a3f37ea966c12
+  HEAD_REF dev-ng
 )
 
 vcpkg_configure_cmake(
-        SOURCE_PATH "${SOURCE_PATH}"
-        PREFER_NINJA
+  SOURCE_PATH "${SOURCE_PATH}"
+  PREFER_NINJA
 )
 
 vcpkg_install_cmake()
@@ -22,6 +22,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE")
 
 file(
-        INSTALL "${SOURCE_PATH}/LICENSE"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-        RENAME copyright)
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
