3f5ed1d92136ced5c82c7e30ccedddb2cadadefb
Charmed Baryon <dev@charmedbaryon.live>
2022-05-24 10:03:04 -0700

    Update FuDGE to 0.3.0, fix port warnings.

--- ports/fully-dynamic-game-engine/portfile.cmake
+++ ports/fully-dynamic-game-engine/portfile.cmake
@@ -7,32 +7,24 @@ vcpkg_from_gitlab(
   GITLAB_URL https://gitlab.com
   OUT_SOURCE_PATH SOURCE_PATH
   REPO colorglass/fully-dynamic-game-engine
-  REF fba85e2f1af40893678b253cfaffb5fb5ae04b3c
-  SHA512 a5eedb7e52cddd0064c1646a52073648b3d6f955a36edb335eeb13eecc89ad5e939febd37d8a51dbf11983b0fdae8325415aa5bf81d312ae91c6ca11c337f428
+  REF 7e2bd3fe4d9effae74014606b12b1640fca43b3a
+  SHA512 9bccc98c9c91b5c2659ecd828a9f069645467cb7c60ae212ac797d61b10cffd898f3b6cf508a297b628ff119d1cf9fe231b952425ee5d0c5877163bd583d5f5a
   HEAD_REF main
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         FEATURES
         "trueflame"             BUILD_TRUEFLAME
-        "trueflame-vr"          BUILD_TRUEFLAME_VR
         "black-book"            BUILD_BLACK_BOOK
-        "black-book-vr"         BUILD_BLACK_BOOK_VR
         "black-book-scripts"    INSTALL_BLACK_BOOK_SCRIPTS
         "hopesfire"             BUILD_HOPESFIRE
-        "hopesfire-vr"          BUILD_HOPESFIRE_VR
-        "trueflame-tests"       BUILD_TRUEFLAME_TESTS
-        "trueflame-tests-vr"    BUILD_TRUEFLAME_TESTS_VR
-        "black-book-tests"      BUILD_BLACK_BOOK_TESTS
-        "black-book-tests-vr"   BUILD_BLACK_BOOK_TESTS_VR
-        "hopesfire-tests"       BUILD_HOPESFIRE_TESTS
-        "hopesfire-tests-vr"    BUILD_HOPESFIRE_TESTS_VR
         )
 
 vcpkg_configure_cmake(
   SOURCE_PATH "${SOURCE_PATH}"
   PREFER_NINJA
   OPTIONS ${FEATURE_OPTIONS}
+  MAYBE_UNUSED_VARIABLES INSTALL_BLACK_BOOK_SCRIPTS
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
@@ -42,6 +34,8 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME FullyDynamicGameEngine CONFIG_PATH share)
 file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/FullyDynamicGameEngine/FullyDynamicGameEngine/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/FullyDynamicGameEngine")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/FullyDynamicGameEngine/FullyDynamicGameEngine")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/FullyDynamicGameEngine.dll")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/lib/FullyDynamicGameEngine.dll")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
@@ -50,14 +44,14 @@ file(
 
 if (INSTALL_BLACK_BOOK_SCRIPTS)
   file(GLOB FDGE_SCRIPTS
-    "${SOURCE_PATH}/contrib/Distribution/Data/Source/Scripts/*.psc"
+    "${SOURCE_PATH}/contrib/Distribution/Scripts/Source/*.psc"
   )
   file(INSTALL ${FDGE_SCRIPTS}
        DESTINATION "${CURRENT_PACKAGES_DIR}/contrib/papyrus/fully-dynamic-game-engine"
   )
 
   file(GLOB FDGE_TEST_SCRIPTS
-    "${SOURCE_PATH}/contrib/Distribution/Data/Source/TestScripts/*.psc"
+    "${SOURCE_PATH}/contrib/PapyrusTestSources/*.psc"
   )
   file(INSTALL ${FDGE_TEST_SCRIPTS}
        DESTINATION "${CURRENT_PACKAGES_DIR}/contrib/papyrus/fully-dynamic-game-engine"
